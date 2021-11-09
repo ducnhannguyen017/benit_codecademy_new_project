99893d9121c75bc0575d6d812e26fe859c2ececd
Duc Nhan Nguyen <ducnhannguyen017@gmail.com>
2021-11-09 10:45:52 +0700

    fix search in postfull

--- src/screens/User/PostFull/PostFull.js
+++ src/screens/User/PostFull/PostFull.js
@@ -1,14 +1,15 @@
 import Header from "components/User/Header/Header";
 import Search from "components/User/Search/Search";
-import React, { useState } from "react";
+import React from "react";
 import PostDetail from "components/User/PostDetail/PostDetail";
 import Footer from "components/User/Footer/Footer";
+import { useHistory } from "react-router";
 
 function PostFull(props) {
   const { match } = props;
-  const [searchTerm, setSearchTerm] = useState("");
+  const history = useHistory();
   const getSearchTerm = (searchTerm) => {
-    setSearchTerm(searchTerm);
+    history.push("/");
   };
 
   return (
